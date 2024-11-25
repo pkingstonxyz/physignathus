9a2b5b95e7cfc1e3db679078707d20c39b47daf0
Patrick Kingston <pkingston@bowdoin.edu>
2024-11-25 15:01:41 -0500

    Got basic server up and running

--- src/physignathus/core.clj
+++ src/physignathus/core.clj
@@ -1,1 +1,25 @@
-(ns physignathus.core)
+(ns physignathus.core
+  (:require [aleph.http :as aleph]
+            ;[muuntaja.core :as muuntaja]
+            [reitit.ring :as ring]))
+            ;[reitit.coercion.malli]))
+            ;[reitit.ring.malli]))
+            ;[malli.util :as mu]))
+
+(def app
+  (ring/ring-handler
+    (ring/router
+      ["/" {:get {:handler (fn [_] {:status 200
+                                    :content-type "text/html"
+                                    :body "Hello"})}}])
+    (ring/routes
+          (ring/create-resource-handler {:path "/"})
+          (ring/create-default-handler
+             {:not-found (constantly {:status 404, :body "404, not found"})
+              :method-not-allowed (constantly {:status 405, :body "405, not allowed"})
+              :not-acceptable (constantly {:status 406, :body "406, not acceptable"})}))))
+
+
+(defn -main
+  [& _args]
+  (aleph/start-server app {:port 8000}))
